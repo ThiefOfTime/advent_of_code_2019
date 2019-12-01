e0e83dc5c52a8535e655743a5c5b905fba07fda0
ThiefOfTime <jan-marius.lenk@cs.tu-dortmund.de>
2019-12-01 21:50:09 +0100

    feat: change to recursive calculation

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -11,19 +11,13 @@ def part_1(data):
 
 
 def part_2(data):
-    if np.sum(data) > 0:
-        res = np.zeros(len(data), dtype=np.int)
-        while np.sum(data) > 0:
-            data = calc_fuel(data)
-            res += data
-        return np.sum(res)
-    else:
-        return 0
+    n_data = calc_fuel(data)
+    return data if np.sum(data) <= 0 else n_data + part_2(n_data)
 
 
 if __name__ == "__main__":
-    inp = list(fileinput.input())
-    inp = [int(x.strip()) for x in inp if len(x.strip()) > 0]
+    with open("input.txt", "r") as file:
+        inp = [int(x) for x in file]
     print(part_1(np.array(inp, dtype=np.int)))
 
-    print(part_2(np.array(inp, dtype=np.int)))
+    print(np.sum(part_2(np.array(inp, dtype=np.int))))
